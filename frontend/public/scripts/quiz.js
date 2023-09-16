3c29043ae00bdb9176329dcbfed181a70c1236fb
SIER0 <73215103+SIER101@users.noreply.github.com>
2023-09-16 21:18:40 +0700

    update 1

--- frontend/public/scripts/quiz.js
+++ frontend/public/scripts/quiz.js
@@ -1,1 +1,7 @@
-//quiz code goes here
+import questions from "./questions.json" assert {type: "json"};
+
+let qusetionNumber = 0;
+
+export function getNextQuestion() {
+    return questions[qusetionNumber++].question;
+}
--- frontend/public/scripts/script.js
+++ frontend/public/scripts/script.js
@@ -1,21 +1,32 @@
-async function loadScoreboard() {
-    const data = await fetch("http://localhost:3222/scoreboard")
-        .then(res => res.json());
-    const itemList = document.getElementById("itemList");
-    itemList.innerHTML = "";
-    for (const player of data) {
-        const newRow = document.createElement("tr");
-        const username = document.createElement("td");
-        username.textContent = player.username;
-        const score = document.createElement("td");
-        score.textContent = player.score;
-        newRow.appendChild(username);
-        newRow.appendChild(score);
-        itemList.appendChild(newRow);
-    }
-}
+import { BACKEND_URL } from "./config.js";
+import { getNextQuestion } from "./quiz.js";
 
 document.addEventListener("DOMContentLoaded", function () {
-    const addItemButton = document.getElementById("get-scoreboard");
-    addItemButton.addEventListener("click", loadScoreboard);
+  const getQuestion = document.getElementById("get-question");
+  getQuestion.addEventListener("click", loadQuestion);
+  const getScoreboard = document.getElementById("get-scoreboard");
+  getScoreboard.addEventListener("click", loadScoreboard);
 });
+
+function loadQuestion() {
+  const question = document.getElementById("question");
+  question.textContent = getNextQuestion();
+}
+
+async function loadScoreboard() {
+  const data = await fetch(`${BACKEND_URL}/scoreboard`).then((res) =>
+    res.json()
+  );
+  const itemList = document.getElementById("itemList");
+  itemList.innerHTML = "";
+  for (const player of data) {
+    const newRow = document.createElement("tr");
+    const username = document.createElement("td");
+    username.textContent = player.username;
+    const score = document.createElement("td");
+    score.textContent = player.score;
+    newRow.appendChild(username);
+    newRow.appendChild(score);
+    itemList.appendChild(newRow);
+  }
+}
