7bb74b937e00da1a5e023c3dbfc149461e2e82db
73215103 <73215103+SIER101@users.noreply.github.com>
2023-10-06 22:19:29 +0700

    add quiz page

--- frontend/public/scripts/quiz.js
+++ frontend/public/scripts/quiz.js
@@ -2,21 +2,22 @@ import questions from "./questions.json" assert {type: "json"};
 
 let questionNumber = -1;
 const question = document.getElementById("question");
+const questionImage = document.getElementById("question-box");
 const btns = document.querySelectorAll('button[id^=answer]');
 
 export function loadNextQuestion() {
-    if(questionNumber + 1 == questions.length)
-    {
+    if (questionNumber + 1 == questions.length) {
         console.log("end");
         return;
     }
     let currQuestion = questions[++questionNumber];
-    question.textContent = currQuestion.question;
-    let i=0
+    //question.textContent = currQuestion.question;
+    questionImage.innerHTML = `<img id="question-image" src="./scripts/images/questions/${currQuestion.image}" alt="${currQuestion.image}">`
+    let i = 0
     btns.forEach(btn => {
         let choices = currQuestion.choices[i++];
-        if(currQuestion.type == "text") btn.innerHTML = choices;
-        else btn.innerHTML = `<img src="./scripts/images/${choices}" alt="${choices}">`;
+        if (currQuestion.type == "text") btn.innerHTML = choices;
+        else btn.innerHTML = `<img id="answers${i - 1}-image" src="./scripts/images/answers/${choices}" alt="${choices}">`;
     });
 };
 
--- frontend/public/scripts/script.js
+++ frontend/public/scripts/script.js
@@ -15,25 +15,25 @@ document.addEventListener("DOMContentLoaded", function () {
   const startButton = document.getElementById("start-button");
   startButton.addEventListener("click", startQuiz)
 
-  /*const btns = document.querySelectorAll('button[id^=answer]');
+  const btns = document.querySelectorAll('button[id^=answer]');
   btns.forEach(btn => {
     btn.addEventListener('click', event => {
-        event.preventDefault()
-        let chosenButton = event.currentTarget;
-        let ButtonId = chosenButton.id
-        let chosenChoice = parseInt(ButtonId.charAt(ButtonId.length - 1));
-        let answer = getAnswer();
-        let answerButtonID = "answer"+ (answer.toString());
-        const answerButton = document.getElementById(answerButtonID);
-        answerButton.className = "correctAnswer";
-        if(chosenChoice != answer) chosenButton.className = "wrongAnswer";
-        setTimeout(function(){
-          chosenButton.className = "";
-          answerButton.className = "";
-          loadNextQuestion();
-        }, 1000);
+      event.preventDefault()
+      let chosenButton = event.currentTarget;
+      let ButtonId = chosenButton.id
+      let chosenChoice = parseInt(ButtonId.charAt(ButtonId.length - 1));
+      let answer = getAnswer();
+      let answerButtonID = "answer" + (answer.toString());
+      const answerButton = document.getElementById(answerButtonID);
+      answerButton.classList.add("correctAnswer");
+      if (chosenChoice != answer) chosenButton.classList.add("wrongAnswer");
+      setTimeout(function () {
+        chosenButton.className = "answer-button";
+        answerButton.className = "answer-button";
+        loadNextQuestion();
+      }, 1000);
     });
-  });*/
+  });
 });
 
 let usernameT = "NaN";
@@ -43,16 +43,16 @@ function submitName() {
   usernameT = usernameInput.value;
   const mainMenu = document.getElementById("main-menu");
   mainMenu.classList = "hide";
-  const introPage = document.getElementById("intro");
+  const introPage = document.getElementById("intro-page");
   introPage.classList = "";
 }
 
 function startQuiz() {
-  const mainMenu = document.getElementById("intro");
+  const mainMenu = document.getElementById("intro-page");
   mainMenu.classList = "hide";
   const introPage = document.getElementById("quiz-page");
   introPage.classList = "";
 }
 
 //main
-//loadNextQuestion()
+loadNextQuestion()
