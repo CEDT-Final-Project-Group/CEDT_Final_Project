ec93b299883189108198f963c03df9effc609b50
73215103 <73215103+SIER101@users.noreply.github.com>
2023-10-12 10:07:40 +0700

    fix server.js

--- frontend/server.js
+++ frontend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
+import { ADDRESS } from "./public/scripts/config.js";
 
 const app = express();
 
 app.use(express.static("public"));
 
 const PORT = 3221;
-app.listen(PORT, "0.0.0.0", () => console.log(`Frontend Server ready on http://localhost:${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Frontend Server ready on ${ADDRESS}:${PORT}`));
